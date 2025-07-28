fb783c572e0c139653530f56a02c30623e463969
unknown <seanter10031999@gmail.com>
2025-07-28 20:19:13 +0700

    Ath

--- lib/app/views/widgets/layout/main_layout.dart
+++ lib/app/views/widgets/layout/main_layout.dart
@@ -5,7 +5,6 @@ import 'package:event_booking_app/app/views/widgets/custom_bottom_navigation_bar
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 class MainLayoutWithTransitions extends StatelessWidget {
   final List<Widget> pages;
   final NavigationController controller = Get.find<NavigationController>();
@@ -47,9 +46,9 @@ class MainLayoutWithTransitions extends StatelessWidget {
         },
       ),
       bottomNavigationBar: Obx(() => CustomBottomNavigationBar(
-        currentIndex: controller.currentIndex.value,
-        onTap: controller.changeTab,
-      )),
+            currentIndex: controller.currentIndex.value,
+            onTap: controller.changeTab,
+          )),
     );
   }
 }
@@ -68,46 +67,49 @@ class StackBasedMainLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Obx(() => AnimatedSwitcher(
-        duration: const Duration(milliseconds: 500),
-        switchInCurve: Curves.easeInOutCubic,
-        switchOutCurve: Curves.easeInOutCubic,
-        transitionBuilder: (child, animation) {
-          // Custom transition based on navigation direction
-          final isForward = controller.currentIndex.value > controller.previousIndex.value;
-          
-          return SlideTransition(
-            position: Tween<Offset>(
-              begin: isForward ? const Offset(1.0, 0.0) : const Offset(-1.0, 0.0),
-              end: Offset.zero,
-            ).animate(
-              CurvedAnimation(
-                parent: animation,
-                curve: Curves.easeInOutCubic,
-              ),
-            ),
-            child: FadeTransition(
-              opacity: Tween<double>(
-                begin: 0.0,
-                end: 1.0,
-              ).animate(
-                CurvedAnimation(
-                  parent: animation,
-                  curve: const Interval(0.3, 1.0, curve: Curves.easeOut),
+            duration: const Duration(milliseconds: 500),
+            switchInCurve: Curves.easeInOutCubic,
+            switchOutCurve: Curves.easeInOutCubic,
+            transitionBuilder: (child, animation) {
+              // Custom transition based on navigation direction
+              final isForward = controller.currentIndex.value >
+                  controller.previousIndex.value;
+
+              return SlideTransition(
+                position: Tween<Offset>(
+                  begin: isForward
+                      ? const Offset(1.0, 0.0)
+                      : const Offset(-1.0, 0.0),
+                  end: Offset.zero,
+                ).animate(
+                  CurvedAnimation(
+                    parent: animation,
+                    curve: Curves.easeInOutCubic,
+                  ),
                 ),
-              ),
-              child: child,
+                child: FadeTransition(
+                  opacity: Tween<double>(
+                    begin: 0.0,
+                    end: 1.0,
+                  ).animate(
+                    CurvedAnimation(
+                      parent: animation,
+                      curve: const Interval(0.3, 1.0, curve: Curves.easeOut),
+                    ),
+                  ),
+                  child: child,
+                ),
+              );
+            },
+            child: Container(
+              key: ValueKey(controller.currentIndex.value),
+              child: pages[controller.currentIndex.value],
             ),
-          );
-        },
-        child: Container(
-          key: ValueKey(controller.currentIndex.value),
-          child: pages[controller.currentIndex.value],
-        ),
-      )),
+          )),
       bottomNavigationBar: Obx(() => CustomBottomNavigationBar(
-        currentIndex: controller.currentIndex.value,
-        onTap: controller.changeTab,
-      )),
+            currentIndex: controller.currentIndex.value,
+            onTap: controller.changeTab,
+          )),
     );
   }
 }
